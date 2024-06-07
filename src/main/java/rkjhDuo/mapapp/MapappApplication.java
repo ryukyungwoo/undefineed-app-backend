d2d8b988bab13f9b221722a6c2994e00390ba87b
ryukyungwoo <ryukyungwoo1220@gmail.com>
2024-06-07 21:25:08 +0900

    chore : [유경우] 스프링 초기설정

---
+++ src/main/java/rkjhDuo/mapapp/MapappApplication.java
@@ -0,0 +1,13 @@
+package rkjhDuo.mapapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rkjhDuo/mapapp/MapappApplicationTests.java
@@ -0,0 +1,13 @@
+package rkjhDuo.mapapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
